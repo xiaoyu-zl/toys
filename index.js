17cb64d31974c99e6cebe6fe87d2bf832434e0dd
Liangjp <17687920152@163.com>
2024-03-13 16:36:18 +0800

    fix: 修复部署路径404问题

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ app.use(express.static("html"));
 const { readDirSync } = require("./registered/htmlRegistered");
 const menu = require("./menu.json");
 let html = readDirSync("html");
-for (const [key, val] of Object.entries(html)) {
-  app.get(key === "/home" ? "/" : key, (req, res) => {
-    res.sendFile(__dirname + val);
-  });
+for ( const [ key,val ] of Object.entries(html) ) {
+    const urlPath = key === "/home" ? "/" : key + '/'
+    
+    app.get(urlPath,( req,res ) => {
+        res.sendFile(__dirname + val);
+    });
 }
-app.get("/menu", (req, res) => {
-  data = menu.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-  res.send({
-    data: data,
-  });
+app.get("/menu",( req,res ) => {
+    data = menu.sort(( a,b ) => Date.parse(b.date) - Date.parse(a.date));
+    res.send({
+        data: data,
+    });
 });
 const translate = require("./router/translate");
-app.use("/translate", translate);
+app.use("/translate",translate);
 const job = require("./router/job");
-app.use("/job", job);
-app.listen(5478, () => {
-  console.log(" http://localhost:5478");
+app.use("/job",job);
+app.listen(5478,() => {
+    console.log(" http://localhost:5478");
 });
